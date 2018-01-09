9be7118966e5c19df0924b147ad13c297422d27b
Chewsterchew <chew@chew.pw>
2018-01-09 17:38:01 -0600

    Add a javascript-based redirect service for CloudCity (neat)

---
+++ redirects.js
@@ -0,0 +1,14 @@
+var href = window.location.href.substring(22);
+if (href == "vote") {
+  window.location = "http://https://forums.cloudcitymc.us/threads/official-server-vote-links.5/";
+}
+if (href == "report") {
+  window.location = "https://forums.cloudcitymc.us/forums/report-players.10/";
+}
+if (href == "sell") {
+  window.location = "https://forums.cloudcitymc.us/threads/items-that-cant-be-should-but-should.9/";
+}
+if (href == "shop") {
+  window.location = "https://forums.cloudcitymc.us/threads/items-that-should-be-sold-in-the-shop.10/";
+}
+window.location = "http://cloudcitymc.us";
